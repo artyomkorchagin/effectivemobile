a5fa4f50951e0138a1e806049ae7d5a4665fb2a8
artyomkorchagin <steamartem@gmail.com>
2025-07-20 22:47:26 +0300

    Combined service methods into 1 file

--- internal/services/subscription/DeleteSubscription.go
+++
@@ -1,9 +0,0 @@
-package servicesubscription
-
-import (
-	"context"
-)
-
-func (s *Service) DeleteSubscription(ctx context.Context, subscriptionID uint64) error {
-	return s.repo.DeleteSubscription(ctx, subscriptionID)
-}
--- internal/services/subscription/service.go
+++ internal/services/subscription/service.go
@@ -1,5 +1,11 @@
 package servicesubscription
 
+import (
+	"context"
+
+	"github.com/artyomkorchagin/effectivemobile/internal/types"
+)
+
 type Service struct {
 	repo ReadWriter
 }
@@ -8,4 +14,28 @@ func NewService(repo ReadWriter) *Service {
 	return &Service{
 		repo: repo,
 	}
+}
+
+func (s *Service) CreateSubscription(ctx context.Context, scr *types.SubscriptionCreateRequest) error {
+	return s.repo.CreateSubscription(ctx, scr)
+}
+
+func (s *Service) DeleteSubscription(ctx context.Context, subscriptionID uint64) error {
+	return s.repo.DeleteSubscription(ctx, subscriptionID)
+}
+
+func (s *Service) GetAllSubscriptions(ctx context.Context) ([]*types.Subscription, error) {
+	return s.repo.GetAllSubscriptions(ctx)
+}
+
+func (s *Service) GetSubscription(ctx context.Context, subscriptionID uint64) (*types.Subscription, error) {
+	return s.repo.GetSubscription(ctx, subscriptionID)
+}
+
+func (s *Service) GetSumOfSubscriptions(ctx context.Context, filter *types.Filter) (uint, error) {
+	return s.repo.GetSumOfSubscriptions(ctx, filter)
+}
+
+func (s *Service) UpdateSubscription(ctx context.Context, sur *types.SubscriptionUpdateRequest) error {
+	return s.repo.UpdateSubscription(ctx, sur)
 }
