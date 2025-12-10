64faf1da4bb67615e881deb918c676ced9d1531c
artyomkorchagin <steamartem@gmail.com>
2025-12-10 20:44:59 +0300

    Fixed filter struct

--- internal/types/filter.go
+++ internal/types/filter.go
@@ -1,10 +1,14 @@
 package types
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Filter struct {
 	UserUUID    *uuid.UUID `form:"user_id" json:"user_id" validate:"uuid"`
 	ServiceName *string    `form:"service_name" json:"service_name"`
-	StartDate   *string    `form:"start_date" json:"start_date"`
-	EndDate     *string    `form:"end_date" json:"end_date"`
+	StartDate   *time.Time `form:"start_date" json:"start_date"`
+	EndDate     *time.Time `form:"end_date" json:"end_date"`
 }
